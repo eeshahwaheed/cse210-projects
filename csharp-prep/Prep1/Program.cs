fb84b206918e900dc82aae72426f0c74cd585426
eeshahwaheed <eeshah.waheednazeer@outlook.com>
2024-09-21 12:56:02 +0500

    one

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string one = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string two = Console.ReadLine();
+        Console.WriteLine($"Your name is {two},{one} {two}. ");
     }
 }
