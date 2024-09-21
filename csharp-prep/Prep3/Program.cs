f2eb370c9cc1c80549dd02e4517d5917f296b2b9
eeshahwaheed <eeshah.waheednazeer@outlook.com>
2024-09-21 14:28:45 +0500

    three

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        Console.WriteLine("What is your guess?");
+        
+        while(guess!=magicnumber)
+        {
+            guess = int.Parse(Console.ReadLine());
+
+            if(guess<magicnumber)
+            { Console.WriteLine("Higher");}
+            else if(guess>magicnumber)
+            { Console.WriteLine("Lower");}
+            else
+            {Console.WriteLine("You guessed it!");}
+            
+        }
+     }
 }
+    
+    
+    
